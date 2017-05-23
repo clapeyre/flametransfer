FIX for issue #13 (rotation) : 	- Changed back to rotation with XYZ aligned mesh 	- Changed the rotation process order : mesh-->metas-->shape 	- Fixed issues with area/volume calculations for the following analytic shapes : Disc, Sphere Also : 	- Fixed iteration on newly defined VectorContainer class for transformations 	- Cleaned the code (moderately)

src/activeflame.py
@@ -300,11 +300,17 @@ class ActiveFlame(object):
             self.metas.transform("scale", *args)
             shape.scale(*args)
         elif trans == "r":
-            self.metas.transform("rotate", *args)
             # Special case: box must be rotated, then interpolated back on a
             # cartesian grid aligned with x,y[,z]
             self._rotate(*args)
+            # transform metas, and overwrite metas for min/max points
+            self.metas.transform("rotate", *args)
+            self.metas.pt_min = Point(self.meshpoints.min(0))
+            self.metas.pt_max = Point(self.meshpoints.max(0))
+            # tranform shape to recover correct json
             shape.rotate(*args)
+            shape.vects.pt_min = Point(self.meshpoints.min(0))
+            shape.vects.pt_max = Point(self.meshpoints.max(0))
         else:
             raise ValueError("Unknown transformation, should be in [t, s, r]")
         self.make_meshpoints()
@@ -314,6 +320,21 @@ class ActiveFlame(object):
         """Convert this flame type to ScatterShape"""
         klass = ScatterShape3D if self.metas.ndim == 3 else ScatterShape2D
         self._shape_metas(klass(self.metas.pt_min, self.metas.pt_max))
+
+    def set_inside_pts(self):
+        """Set points inside flame according to flame file"""
+        with File(self.flame_file, 'r') as flame :
+            self.inside_points = flame["/Average/flames"].value
+
+    def read_mesh(self):
+        """read coordinates of the current mesh"""
+        Mshcor = np.zeros((self.metas.grid_size**self.metas.ndim, self.metas.ndim))
+        with File(self.mesh_file, 'r') as mesh:
+            Mshcor[:,0]  = mesh["/Coordinates/x"].value
+            Mshcor[:,1]  = mesh["/Coordinates/y"].value
+            if (self.metas.ndim == 3):
+                Mshcor[:,2]  = mesh["/Coordinates/z"].value
+        return Mshcor
 
     def _rotate(self, axis, angle, degrees=True):
         """Write and rotate flame in hip, then recreate x,y[,z] aligned mesh"""
@@ -328,25 +349,33 @@ class ActiveFlame(object):
           """.format(self, axis, angle, self.name))
         self.hip_wrapper.execute(rotate_script)
         self.harmonize_flame_name()
+        self.set_inside_pts()
+        Mshcor = self.read_mesh()
+        flamepoints = Mshcor[(self.inside_points>0.01)]
 
-        with File(self.flame_file, 'r') as flame:
-            self.inside_points = flame["/Average/flames"].value
-        self.make_meshpoints()
-        flamepoints = self.meshpoints[self.inside_points > 0.01]
         self.metas.pt_min = Point(flamepoints.min(0))
         self.metas.pt_max = Point(flamepoints.max(0))
+        self.make_meshpoints()
 
         interp_script = ["re hd -a {0.mesh_file} -s {0.flame_file}"
                          .format(self)]
         interp_script += self._get_hip_script_generate().split("\n")
-        interp_script.insert(3, "in gr 1")
+        interp_script.insert(3, "se in-rim 0")
+        interp_script.insert(4, "in gr 1")
+#       WAITING FOR HIP INTERPOLATION BUG CORRECTION
+#       interp_script.insert(3, "se ch 0")
+#       interp_script.insert(4, "se in-recoType flag")
+#       interp_script.insert(5, "se in-rim 0")
+#       interp_script.insert(6, "se bc-ty * n")
+#       interp_script.insert(7, "in gr 1")
         self.hip_wrapper.execute('\n'.join(interp_script))
         self.harmonize_flame_name()
-        self.write_h5()
+        self.set_inside_pts()
+        self.write_h5() 
 
     def export_avsp(self, avsp_mesh, avsp_sol, number):
         """Export flame to AVSP solution"""
-        self.make_mesh()
+	self.make_mesh()
         name = "flame_{}.h5".format(number)
         self.write_h5(number=number, name=name)
         # with File("avsp.sol.h5", "r") as f: print f["Average/flames"].value

src/geometry.py
@@ -89,6 +89,9 @@ class VectorContainer(object):
         assert issubclass(value.__class__, Vector)
         self.__dict__[name] = value
 
+    def __iter__(self):
+	for value in self.__dict__.values():
+	    yield value
 
 class Shape(object):
     def __init__(self, ndim):
@@ -96,13 +99,13 @@ class Shape(object):
         self.vects = VectorContainer()
 
     def rotate(self, axis, angle, degrees=True):
-        [p.rotate(axis, angle, degrees) for p in self.vects()]
+        [p.rotate(axis, angle, degrees) for p in self.vects]
 
     def scale(self, vect):
-        [p.scale(vect) for p in self.vects()]
+        [p.scale(vect) for p in self.vects]
 
     def translate(self, vect):
-        [p.translate(vect) for p in self.vects()]
+        [p.translate(vect) for p in self.vects]
 
 
 class Shape2D(Shape):
@@ -328,7 +331,7 @@ class Parallelepiped(Shape3D):
         num_b = np.apply_along_axis(
             lambda m: np.linalg.det(
                 np.vstack([self.mat[:, 0], m, self.mat[:, 2]])), 1, x)
-        num_c = np.apply_along_axis(
+	num_c = np.apply_along_axis(
             lambda m: np.linalg.det(
                 np.vstack([self.mat[:, 0], self.mat[:, 1], m])), 1, x)
         return num_a / den, num_b / den, num_c / den
@@ -342,24 +345,29 @@ class Parallelepiped(Shape3D):
 class Disc(Shape2D):
     def __init__(self, center, radius):
         Shape2D.__init__(self)
+	# radius is read from input as a float and must be converted to array to comply with defined transformations
+	radius = np.ravel(radius)
+	if (len(radius)==1):
+	        self.vects.radius = Vector((np.append(radius, 0.0).reshape(-1,1)))
+	else :
+		self.vects.radius = Vector(radius)	
         self.vects.center = Point(center)
-        self.radius = np.array(radius)
         self.check()
         self.bounding_box()
 
     def bounding_box(self):
         """Set bounding box for shape"""
-        self.vects.pt_min = Point(self.vects.center() - self.radius)
-        self.vects.pt_max = Point(self.vects.center() + self.radius)
+	self.vects.pt_min = Point(self.vects.center() - np.linalg.norm(self.vects.center()))
+        self.vects.pt_max = Point(self.vects.center() + np.linalg.norm(self.vects.center()))
 
     @property
     def args(self):
-        return [self.vects.center(), self.radius]
+        return [self.vects.center(), self.vects.radius()]
 
     @property
     def area(self):
         """Get disc area"""
-        return np.pi * self.radius**2
+	return np.pi * (np.linalg.norm(self.vects.center()))**2
     volume = area
 
     def distance(self, x):
@@ -368,30 +376,33 @@ class Disc(Shape2D):
 
     def is_inside(self, x):
         """Determine if x is inside the disc"""
-        return (self.distance(x) <= self.radius)
-
+	return(self.distance(x) <= np.linalg.norm(self.vects.radius()))
 
 class Sphere(Shape3D):
     def __init__(self, center, radius):
         Shape3D.__init__(self)
         self.vects.center = Point(center)
-        self.radius = np.array(radius)
+	radius = np.ravel(radius)
+        if (len(radius)==1):
+                self.vects.radius = Vector((np.append(radius, [0.0, 0.0]).reshape(-1,1)))
+        else :
+                self.vects.radius = Vector(radius)
         self.check()
         self.bounding_box()
 
     def bounding_box(self):
         """Set bounding box for shape"""
-        self.vects.pt_min = Point(self.vects.center() - self.radius)
-        self.vects.pt_max = Point(self.vects.center() + self.radius)
+	self.vects.pt_min = Point(self.vects.center() - np.linalg.norm(self.vects.radius()))
+        self.vects.pt_max = Point(self.vects.center() + np.linalg.norm(self.vects.radius()))
 
     @property
     def args(self):
-        return [self.vects.center(), self.radius]
+        return [self.vects.center(), self.vects.radius()]
 
     @property
     def volume(self):
         """Get sphere volume"""
-        return (4./3. * np.pi * self.radius**3)[0]
+	return (4./3. * np.pi * np.linalg.norm(self.vects.radius())**3)
 
     def distance(self, x):
         """Find distance to sphere center"""
@@ -399,8 +410,7 @@ class Sphere(Shape3D):
 
     def is_inside(self, x):
         """Determine if x is inside the sphere"""
-        return (self.distance(x) <= self.radius)
-
+	return(self.distance(x) <= np.linalg.norm(self.vects.radius()))
 
 class Cylinder(Shape3D):
     def __init__(self, center, radius, ax_vec):