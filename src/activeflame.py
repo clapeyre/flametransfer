clapeyre/flametransfer

commit af53e7bfbbbd64b7b7b195497d5e4a77a64dddce

Cleaning after #12 fix.

src/activeflame.py
@@ -326,20 +326,26 @@ class ActiveFlame(object):
         with File(self.flame_file, 'r') as flame :
             self.inside_points = flame["/Average/flames"].value
 
-    def read_mesh(self):
-        """read coordinates of the current mesh"""
-        Mshcor = np.zeros((self.metas.grid_size**self.metas.ndim, self.metas.ndim))
+    def read_meshpoints(self):
+        """Read coordinates of the current mesh.
+
+        This mesh might not match the FlameTransfer format (e.g. cartesian
+        aligned) so self.meshpoints is not updated.
+        """
+        meshpoints = np.zeros((self.metas.grid_size**self.metas.ndim, self.metas.ndim))
         with File(self.mesh_file, 'r') as mesh:
-            Mshcor[:,0]  = mesh["/Coordinates/x"].value
-            Mshcor[:,1]  = mesh["/Coordinates/y"].value
+            meshpoints[:,0]  = mesh["/Coordinates/x"].value
+            meshpoints[:,1]  = mesh["/Coordinates/y"].value
             if (self.metas.ndim == 3):
-                Mshcor[:,2]  = mesh["/Coordinates/z"].value
-        return Mshcor
+                meshpoints[:,2]  = mesh["/Coordinates/z"].value
+        return meshpoints
 
     def _rotate(self, axis, angle, degrees=True):
         """Write and rotate flame in hip, then recreate x,y[,z] aligned mesh"""
         self.make_mesh()
         self.write_h5()
+
+        # Perform rotation of the mesh with hip
         if not degrees:
             angle *= 180./np.pi
         rotate_script = dedent("""\
@@ -350,32 +356,33 @@ class ActiveFlame(object):
         self.hip_wrapper.execute(rotate_script)
         self.harmonize_flame_name()
         self.set_inside_pts()
-        Mshcor = self.read_mesh()
-        flamepoints = Mshcor[(self.inside_points>0.01)]
 
+        # Extract all flame point coordinates to create new cartesian aligned
+        # bounding box
+        meshpoints = self.read_meshpoints()
+        flamepoints = meshpoints[self.inside_points > 0.01]
         self.metas.pt_min = Point(flamepoints.min(0))
         self.metas.pt_max = Point(flamepoints.max(0))
         self.make_meshpoints()
 
+        # Interpolate the rotated flame on the new cartesian aligned grid
         interp_script = ["re hd -a {0.mesh_file} -s {0.flame_file}"
                          .format(self)]
         interp_script += self._get_hip_script_generate().split("\n")
-        interp_script.insert(3, "se in-rim 0")
-        interp_script.insert(4, "in gr 1")
-#       WAITING FOR HIP INTERPOLATION BUG CORRECTION
-#       interp_script.insert(3, "se ch 0")
-#       interp_script.insert(4, "se in-recoType flag")
-#       interp_script.insert(5, "se in-rim 0")
-#       interp_script.insert(6, "se bc-ty * n")
-#       interp_script.insert(7, "in gr 1")
+        interp_script += ["se ch 0",
+                          "se in-rim 0",
+                          # WAITING FOR HIP INTERPOLATION BUG CORRECTION
+                          # "se bc-ty * n",
+                          # "se in-recoType flag",
+                          "in gr 1"]
         self.hip_wrapper.execute('\n'.join(interp_script))
         self.harmonize_flame_name()
         self.set_inside_pts()
-        self.write_h5() 
+        self.write_h5()
 
     def export_avsp(self, avsp_mesh, avsp_sol, number):
         """Export flame to AVSP solution"""
-	self.make_mesh()
+        self.make_mesh()
         name = "flame_{}.h5".format(number)
         self.write_h5(number=number, name=name)
         # with File("avsp.sol.h5", "r") as f: print f["Average/flames"].value

src/geometry.py
@@ -90,8 +90,8 @@ class VectorContainer(object):
         self.__dict__[name] = value
 
     def __iter__(self):
-	for value in self.__dict__.values():
-	    yield value
+        for value in self.__dict__.values():
+            yield value
 
 class Shape(object):
     def __init__(self, ndim):
@@ -331,7 +331,7 @@ class Parallelepiped(Shape3D):
         num_b = np.apply_along_axis(
             lambda m: np.linalg.det(
                 np.vstack([self.mat[:, 0], m, self.mat[:, 2]])), 1, x)
-	num_c = np.apply_along_axis(
+        num_c = np.apply_along_axis(
             lambda m: np.linalg.det(
                 np.vstack([self.mat[:, 0], self.mat[:, 1], m])), 1, x)
         return num_a / den, num_b / den, num_c / den
@@ -344,34 +344,37 @@ class Parallelepiped(Shape3D):
 
 class Disc(Shape2D):
     def __init__(self, center, radius):
+        """Read radius from input as a float, convert to array to comply with
+        defined transformations (otherwise *scaling* would fail to affect
+        radius)
+        """
         Shape2D.__init__(self)
-	# radius is read from input as a float and must be converted to array to comply with defined transformations
         try:
-            assert len(radius)==1
-            self.vects.radius = Vector(np.array([radius[0], 0]).reshape(-1,1))
-        except :
-            self.vects.radius = Vector(radius)        
-	self.vects.center = Point(center)
+            assert len(radius) == 1
+            self.vects.radius = Vector(np.array([radius[0], 0]))
+        except TypeError:
+            self.vects.radius = Vector(np.array([radius, 0]))
+        self.vects.center = Point(center)
         self.check()
         self.bounding_box()
 
     @property
     def radius(self):
-	return self.vects.radius.length
+        return self.vects.radius.length
 
     def bounding_box(self):
         """Set bounding box for shape"""
-	self.vects.pt_min = Point(self.vects.center() - self.radius)
+        self.vects.pt_min = Point(self.vects.center() - self.radius)
         self.vects.pt_max = Point(self.vects.center() + self.radius)
 
     @property
     def args(self):
-        return [self.vects.center(), self.vects.radius()]
+        return [self.vects.center(), self.radius]
 
     @property
     def area(self):
         """Get disc area"""
-	return np.pi * (self.radius)**2
+        return np.pi * self.radius**2
     volume = area
 
     def distance(self, x):
@@ -380,37 +383,38 @@ class Disc(Shape2D):
 
     def is_inside(self, x):
         """Determine if x is inside the disc"""
-	return(self.distance(x) <= self.radius)
+        return (self.distance(x) <= self.radius)
+
 
 class Sphere(Shape3D):
     def __init__(self, center, radius):
         Shape3D.__init__(self)
         self.vects.center = Point(center)
-	try:
-	    assert len(radius)==1
-            self.vects.radius = Vector(np.array([radius[0], 0, 0]).reshape(-1,1))
-	except :
-	    self.vects.radius = Vector(radius)
+        try:
+            assert len(radius) == 1
+            self.vects.radius = Vector(np.array([radius[0], 0, 0]))
+        except :
+            self.vects.radius = Vector(np.array([radius, 0, 0]))
         self.check()
         self.bounding_box()
 
-    @property 
+    @property
     def radius(self):
-	return self.vects.radius.length
+        return self.vects.radius.length
 
     def bounding_box(self):
         """Set bounding box for shape"""
-	self.vects.pt_min = Point(self.vects.center() - self.radius)
+        self.vects.pt_min = Point(self.vects.center() - self.radius)
         self.vects.pt_max = Point(self.vects.center() + self.radius)
 
     @property
     def args(self):
-        return [self.vects.center(), self.vects.radius()]
+        return [self.vects.center(), self.radius]
 
     @property
     def volume(self):
         """Get sphere volume"""
-	return (4./3. * np.pi * self.radius**3)
+        return 4./3. * np.pi * self.radius**3
 
     def distance(self, x):
         """Find distance to sphere center"""
@@ -418,13 +422,14 @@ class Sphere(Shape3D):
 
     def is_inside(self, x):
         """Determine if x is inside the sphere"""
-	return(self.distance(x) <= self.radius)
+        return (self.distance(x) <= self.radius)
+
 
 class Cylinder(Shape3D):
     def __init__(self, center, radius, ax_vec):
         Shape3D.__init__(self)
         self.vects.xref = Point(center)
-        self.vects.axis = Vector(ax_vec)
+        self.vects.ax_vec = Vector(ax_vec)
         self.radius = np.array(radius)
         self.check()
         self.bounding_box()

commit 3a69fd3de6576e1bbf5fe6361c28f9121672377c

Fix for a bug introduced in af53e7bf
    - fixed a bug with the hip script generation for rotations
    - extended the "radius vector" approach to cylinders

src/activeflame.py
@@ -277,10 +277,9 @@ class ActiveFlame(object):
                    if self.metas.ndim == 3 else "")
         hip_script = dedent("""\
           ge {0[0]} {0[1]} {1[0]} {1[1]} {2} {2}
-          {3}
-          wr hd ./{4}
+          {3} 
           """).format(self.metas.pt_min, self.metas.pt_max,
-                      self.metas.grid_size, line_3d, self.name)
+                      self.metas.grid_size, line_3d)
         return hip_script
 
     def transform(self, trans, *args):
@@ -374,7 +373,8 @@ class ActiveFlame(object):
                           # WAITING FOR HIP INTERPOLATION BUG CORRECTION
                           # "se bc-ty * n",
                           # "se in-recoType flag",
-                          "in gr 1"]
+                          "in gr 1",
+                          "wr hd ./{0}".format(self.name)]         
         self.hip_wrapper.execute('\n'.join(interp_script))
         self.harmonize_flame_name()
         self.set_inside_pts()

src/geometry.py
@@ -428,9 +428,16 @@ class Cylinder(Shape3D):
         Shape3D.__init__(self)
         self.vects.xref = Point(center)
         self.vects.ax_vec = Vector(ax_vec)
-        self.radius = np.array(radius)
+        try:
+            self.vects.radius = Vector(np.array([radius[0], 0]))
+        except TypeError:
+            self.vects.radius = Vector(np.array([radius, 0]))
         self.check()
         self.bounding_box()
+
+    @property
+    def radius(self):
+        return self.vects.radius.length
 
     def bounding_box(self):
         """Set bounding box for shape"""
@@ -443,7 +450,7 @@ class Cylinder(Shape3D):
                         xref_axis - proj,
                         xref_axis + vec_axis + proj,
                         xref_axis + vec_axis - proj]
-            return min(four_pts)[0], max(four_pts)[0]
+            return min(four_pts), max(four_pts)
         xmin, xmax = min_max_cyl(0)
         ymin, ymax = min_max_cyl(1)
         zmin, zmax = min_max_cyl(2)
@@ -458,7 +465,7 @@ class Cylinder(Shape3D):
     def volume(self):
         """Get cylinder volume"""
         return (np.linalg.norm(self.vects.ax_vec()) * np.pi
-                * self.radius**2)[0]
+                * self.radius**2)
 
     def project(self, x):
         """Project vector x on basis formed by cylinder